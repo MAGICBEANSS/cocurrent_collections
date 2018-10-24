b6941a6adcb7da8914099d427e5cb63b6f4427bd
pp_mb <piyush.pandey@muraai.com>
2018-10-24 22:40:00 +0530

    arraylist_concurrent_modification_Exception

---
+++ src/concurrent_collections/ConcurrentList.java
@@ -0,0 +1,53 @@
+package concurrent_collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class ConcurrentList {
+	static ArrayList<Integer> al = new ArrayList<>();
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ConcurrentList.al.add(1);
+		ConcurrentList.al.add(2);
+		ConcurrentList.al.add(3);
+		ConcurrentList.al.add(4);
+		ConcurrentList.al.add(5);
+		Iterator<Integer> it =  al.iterator();
+MyThread mt = new MyThread();
+Thread t2 = new Thread(mt);
+t2.start();
+		while(it.hasNext())
+		{
+			Integer z = it.next();
+			System.out.println("z "+z);
+			Thread.currentThread().sleep(3000);
+			
+		}
+	//	al = (ArrayList<Integer>) Collections.synchronizedList(al);
+		
+
+	}
+
+}
+
+class MyThread implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("child thread");
+			ConcurrentList.al.add(12);
+			Thread.currentThread().sleep(2000);
+		} catch (InterruptedException e) {
+			System.out.println("child thread");
+				ConcurrentList.al.add(12);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+	}
+	
+}

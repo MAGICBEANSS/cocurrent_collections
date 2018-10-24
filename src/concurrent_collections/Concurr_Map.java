c4ac84e9c85e880aafa8e3f04be0806a350ce034
pp_mb <piyush.pandey@muraai.com>
2018-10-25 01:07:35 +0530

    replace(K,V)_ConcurrentHashMap

--- src/concurrent_collections/Concurr_Map.java
+++ src/concurrent_collections/Concurr_Map.java
@@ -11,8 +11,10 @@ public class Concurr_Map {
 		chm.putIfAbsent(1, "abc");
 		chm.putIfAbsent(2, "abc");
 		chm.replace(1, "def","zzz");
+		chm.replace(4, "def","zzz");
 		System.out.println("1 chm "+chm);
 		chm.replace(1, "abc","zzz");
+		chm.replace(3, "abc","zzz");
 		System.out.println("2 chm "+chm);
 	}
 
